86545e418106f179cfcd096c8949914cb4d9d1a6
RunsenFeng <fengruns@mail.ustc.edu.cn>
2025-03-01 14:42:47 +0800

    Minor update

--- image/main.py
+++ image/main.py
@@ -13,19 +13,6 @@ class GlobalStepRichProgressBar(RichProgressBar):
         return self.trainer.max_steps - self.trainer.global_step
 
 
-class ClearLogdir(Callback):
-    def on_fit_start(self, trainer, pl_module):
-        log_dir = Path(trainer.logger.log_dir)
-        if log_dir.exists():
-            targets = log_dir.glob("*")
-            for target in targets:
-                if target.is_dir():
-                    shutil.rmtree(target)
-                elif target.is_file():
-                    target.unlink()
-        Path(trainer.log_dir).mkdir(exist_ok=True, parents=True)
-
-
 class MyLightningCLI(LightningCLI):
     def after_fit(self):
         self.trainer.test(self.model, self.datamodule)
@@ -40,5 +27,7 @@ if __name__ == '__main__':
         subclass_mode_model=True,
         subclass_mode_data=True,
         save_config_kwargs={"overwrite": True},
-        # parser_kwargs={"parser_mode": "omegaconf"},
+        trainer_defaults = {
+            "callbacks": [lazy_instance(GlobalStepRichProgressBar)],
+        }
     )
